cb66737ec1adea4798c24878a5b77d7538082dd8
Gabriel Silvestre <47044671+gabriel200405@users.noreply.github.com>
2025-11-28 20:23:29 -0300

    Update app.java

--- src/app.java
+++ src/app.java
@@ -1,1 +1,13 @@
+public class app {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int resultado = dividir(a, b);
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public static int dividir(int x, int y) {
+        return x / y;  
+    }
+}
 
