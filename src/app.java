04c2e4b75898020c6cb37822ea2155c563f2e7ba
Gabriel Silvestre da Silva <gabrielsilvestre559@gmail.com>
2025-11-28 20:36:04 -0300

    fix: corrige erro crítico de execução

--- src/app.java
+++ src/app.java
@@ -1,7 +1,7 @@
 public class app {
     public static void main(String[] args) {
         int a = 10;
-        int b = 0;
+        int b = 2;
         int resultado = dividir(a, b);
         System.out.println("Resultado: " + resultado);
     }
